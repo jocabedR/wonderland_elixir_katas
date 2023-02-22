17f679889a1d0ff353b9b91a52e622c455403f82
Jocabed Ríos <j0c4b3d@gmail.com>
2023-02-22 13:58:50 -0600

    Adding functions to CardGameWar.Game to pass test cases.

--- card_game_war/lib/game.ex
+++ card_game_war/lib/game.ex
@@ -1,13 +1,69 @@
 defmodule CardGameWar.Game do
-
   # feel free to use these cards or use your own data structure"
-  def suits, do: [:spade, :club, :diamond, :heart]
-  def ranks, do: [2, 3, 4, 5, 6, 7, 8, 9, 10, :jack, :queen, :king, :ace]
-  def cards do
-    for suit <- suits(),
-        rank <- ranks() do
-      {suit, rank}
+  @suits [:spade, :club, :diamond, :heart]
+  @ranks [2, 3, 4, 5, 6, 7, 8, 9, 10, :jack, :queen, :king, :ace]
+  @cards for suit <- @suits, rank <- @ranks, do: {suit, rank}
+
+  def ranks(), do: @ranks
+
+  defstruct deck_p1: [], deck_p2: []
+
+  def new(n \\ 26)
+
+  def new(n) when n > 0 and n < 27 do
+    cards = @cards |> Enum.shuffle() |> Enum.shuffle() |> Enum.shuffle()
+    deck_p1 = Enum.take(cards, n)
+    deck_p2 = Enum.drop(cards, 26) |> Enum.take(n)
+    %__MODULE__{deck_p1: deck_p1, deck_p2: deck_p2}
+  end
+
+  def play(%__MODULE__{deck_p1: []}), do: :player2
+  def play(%__MODULE__{deck_p2: []}), do: :player1
+
+  def play(
+        %__MODULE__{
+          deck_p1: [card_p1 | other_cards_p1],
+          deck_p2: [card_p2 | other_cards_p2]
+        } = game
+      ) do
+    case round_winner(card_p1, card_p2) do
+      :player1 ->
+        cards_p1 = other_cards_p1 ++ [card_p1, card_p2]
+        play(%{game | deck_p1: cards_p1, deck_p2: other_cards_p2})
+
+      :player2 ->
+        cards_p2 = other_cards_p2 ++ [card_p2, card_p1]
+        play(%{game | deck_p1: other_cards_p1, deck_p2: cards_p2})
+
+      error ->
+        throw(error)
     end
   end
 
+  def round_winner({suit_p1, rank_p1}, {suit_p2, rank_p2})
+      when suit_p1 in @suits and
+             rank_p1 in @ranks and
+             suit_p2 in @suits and
+             rank_p2 in @ranks do
+    rel_val1 = Enum.find_index(@ranks, &(&1 == rank_p1))
+    rel_val2 = Enum.find_index(@ranks, &(&1 == rank_p2))
+
+    cond do
+      rel_val1 > rel_val2 ->
+        :player1
+
+      rel_val1 < rel_val2 ->
+        :player2
+
+      true ->
+        suit_rel_val1 = Enum.find_index(@suits, &(&1 == suit_p1))
+        suit_rel_val2 = Enum.find_index(@suits, &(&1 == suit_p2))
+
+        cond do
+          suit_rel_val1 > suit_rel_val2 -> :player1
+          suit_rel_val1 < suit_rel_val2 -> :player2
+          true -> {:error, "You can not have the same card."}
+        end
+    end
+  end
 end
--- card_game_war/test/game_test.exs
+++ card_game_war/test/game_test.exs
@@ -1,36 +1,38 @@
 defmodule CardGameWar.GameTest do
   use ExUnit.Case
+  import CardGameWar.Game
 
   test "the highest rank wins the cards in the round" do
-    flunk("not implemented")
+    assert :player1 == round_winner({:heart, :king}, {:heart, 2})
+    assert :player2 == round_winner({:heart, 2}, {:spade, :king})
   end
 
   test "queens are higher rank than jacks" do
-    flunk("not implemented")
+    assert :player1 == round_winner({:heart, :queen}, {:heart, :jack})
   end
 
   test "kings are higher rank than queens" do
-    flunk("not implemented")
+    assert :player2 == round_winner({:heart, :queen}, {:spade, :king})
   end
 
   test "aces are higher rank than kings" do
-    flunk("not implemented")
+    assert :player1 == round_winner({:spade, :ace}, {:spade, :king})
   end
 
   test "if the ranks are equal, clubs beat spades" do
-    flunk("not implemented")
+    assert :player1 == round_winner({:club, :ace}, {:spade, :ace})
   end
 
   test "if the ranks are equal, diamonds beat clubs" do
-    flunk("not implemented")
+    assert :player2 == round_winner({:club, 2}, {:diamond, 2})
   end
 
   test "if the ranks are equal, hearts beat diamonds" do
-    flunk("not implemented")
+    assert :player1 == round_winner({:heart, :jack}, {:diamond, :jack})
   end
 
   test "the player loses when they run out of cards" do
-    flunk("not implemented")
+    game = %CardGameWar.Game{deck_p1: [], deck_p2: [diamond: :queen, spade: 7]}
+    assert :player2 = play(game)
   end
-
 end
