a0f578f49ea49e81c055155a442af02dbaa077eb
Jocabed Ríos <j0c4b3d@gmail.com>
2023-02-23 10:59:34 -0600

    Adding Doublets solution and tests.

--- doublets/lib/solver.ex
+++ doublets/lib/solver.ex
@@ -1,13 +1,50 @@
 defmodule Doublets.Solver do
-
-  def words do
-    "./resources/words.txt"
-      |> File.stream!
-      |> Enum.to_list
-  end
+  @words "./resources/words.txt"
+         |> File.stream!()
+         |> Enum.to_list()
+         |> Enum.map(&String.replace(&1, "\n", ""))
 
   def doublets(word1, word2) do
-    "make me work"
+    doublets_impl([[word1]], word2)
   end
 
+  def doublets_impl([], _word2), do: []
+
+  def doublets_impl(word_seq, word2) do
+    seq_variants = Enum.flat_map(word_seq, fn seq -> complete_seq_variants(seq) end)
+
+    case find_solution(seq_variants, word2) do
+      nil -> doublets_impl(seq_variants, word2)
+      sol -> sol
+    end
+  end
+
+  def find_solution(word_seq, target) do
+    Enum.find(word_seq, &(last_word(&1) == target))
+  end
+
+  def complete_seq_variants(word_seq) do
+    variants =
+      word_seq
+      |> last_word()
+      |> possible_next()
+      |> Enum.filter(&(&1 not in word_seq))
+
+    for v <- variants, do: word_seq ++ [v]
+  end
+
+  def last_word(words), do: Enum.at(words, -1)
+
+  def possible_next(word) do
+    Enum.filter(@words, &(String.length(&1) == String.length(word)))
+    |> Enum.filter(&(distance(word, &1) == 1))
+  end
+
+  def distance(word1, word2) do
+    word1_chars = String.graphemes(word1)
+    word2_chars = String.graphemes(word2)
+
+    Enum.zip_with(word1_chars, word2_chars, fn x, y -> x != y end)
+    |> Enum.count(& &1)
+  end
 end
--- doublets/test/solver_test.exs
+++ doublets/test/solver_test.exs
@@ -4,19 +4,43 @@ defmodule Doublets.SolverTest do
 
   test "with word links found" do
     assert ["head", "heal", "teal", "tell", "tall", "tail"] ==
-           doublets("head", "tail")
+             doublets("head", "tail")
 
     assert ["door", "boor", "book", "look", "lock"] ==
-           doublets("door", "lock")
+             doublets("door", "lock")
 
     assert ["bank", "bonk", "book", "look", "loon", "loan"] ==
-           doublets("bank", "loan")
+             doublets("bank", "loan")
 
     assert ["wheat", "cheat", "cheap", "cheep", "creep", "creed", "breed", "bread"] ==
-           doublets("wheat", "bread")
+             doublets("wheat", "bread")
   end
 
   test "with no word links found" do
     assert [] == doublets("ye", "freezer")
+  end
+
+  test "last_word returns the last word in a list" do
+    assert "bread" =
+             last_word(["wheat", "cheat", "cheap", "cheep", "creep", "creed", "breed", "bread"])
+  end
+
+  test "distance returns the distance between two words" do
+    assert 1 = distance("wheat", "cheat")
+    assert 4 = distance("head", "tail")
+    assert 3 = distance("wheat", "bread")
+  end
+
+  test "possible_next returns the all the words with the same length and a distance of 1" do
+    assert ["cheat", "cheep"] = possible_next("cheap")
+    assert ["creep", "breed"] = possible_next("creed")
+  end
+
+  test "complete_seq_variants returns a list of lists where each list contains \"the list\" and one of the variants of the last word of a list that was not in \"the list\"" do
+    assert [["creep", "creed", "breed"]] = complete_seq_variants(["creep", "creed"])
+  end
+
+  test "find_solution returns a list whose last word is equal to the second argument we sent" do
+    assert ["creed", "breed"] = find_solution([["creed", "creep"], ["creed", "breed"]], "breed")
   end
 end
